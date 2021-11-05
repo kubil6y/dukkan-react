b617669a377bdd8b9d8db46459d7b78bae717e35
Kubilay Humm <lieqb2@gmail.com>
2021-11-05 11:33:10 +0300

    gm

--- src/components/navbar/SearchInput.tsx
+++ src/components/navbar/SearchInput.tsx
@@ -7,65 +7,71 @@ import { Flex, Icon, Tooltip, Center, Input } from "@chakra-ui/react";
 export const SearchInput: FC = () => {
   const [inputFocused, setInputFocused] = useState(false);
   return (
-    <Flex
-      w="65%"
-      my="6px"
-      mx="auto"
-      borderRadius="4px"
-      alignItems="center"
-      overflow="hidden"
+    <form
       style={{
-        ...(inputFocused && {
-          outlineColor: colors.orangeTernary,
-          outlineStyle: "solid",
-          outlineWidth: "3px",
-        }),
+        width: "65%",
       }}
     >
-      <Tooltip
-        label="Work in Progress..."
-        bg={colors.lightGrayPrimary}
-        color={colors.darkGrayPrimary}
-        fontSize="13px"
+      <Flex
+        w="65%"
+        my="6px"
+        mx="auto"
+        borderRadius="4px"
+        alignItems="center"
+        overflow="hidden"
+        style={{
+          ...(inputFocused && {
+            outlineColor: colors.orangeTernary,
+            outlineStyle: "solid",
+            outlineWidth: "3px",
+          }),
+        }}
       >
-        <Flex
+        <Tooltip
+          label="Work in Progress..."
           bg={colors.lightGrayPrimary}
-          justifyContent="center"
-          alignItems="center"
-          px="10px"
-          height="100%"
-          _hover={{ cursor: "pointer" }}
+          color={colors.darkGrayPrimary}
+          fontSize="13px"
         >
-          <Center fontSize="12px">All</Center>
-          <Icon
-            as={IoMdArrowDropdown}
-            w={4}
-            h={4}
-            color="gray.500"
-            marginLeft="6px"
+          <Flex
+            bg={colors.lightGrayPrimary}
+            justifyContent="center"
+            alignItems="center"
+            px="10px"
+            height="100%"
+            _hover={{ cursor: "pointer" }}
+          >
+            <Center fontSize="12px">All</Center>
+            <Icon
+              as={IoMdArrowDropdown}
+              w={4}
+              h={4}
+              color="gray.500"
+              marginLeft="6px"
+            />
+          </Flex>
+        </Tooltip>
+        <Flex width="100%">
+          <Input
+            type="text"
+            bg="white"
+            borderRadius="0"
+            placeholder="Search for Products"
+            _focus={{ borderColor: "none" }}
+            onFocus={() => setInputFocused(true)}
+            onBlur={() => setInputFocused(false)}
           />
         </Flex>
-      </Tooltip>
-      <Flex width="100%">
-        <Input
-          type="text"
-          bg="white"
-          borderRadius="0"
-          placeholder="Search for Products"
-          _focus={{ borderColor: "none" }}
-          onFocus={() => setInputFocused(true)}
-          onBlur={() => setInputFocused(false)}
-        />
+        <Center
+          bg={colors.orangeSecondary}
+          h="100%"
+          px="12px"
+          borderRightRadius="4px"
+          cursor="pointer"
+        >
+          <Icon as={GoSearch} w={5} h={5} color={colors.darkGrayPrimary} />
+        </Center>
       </Flex>
-      <Center
-        bg={colors.orangeSecondary}
-        h="100%"
-        px="12px"
-        borderRightRadius="4px"
-        cursor="pointer"
-      >
-        <Icon as={GoSearch} w={5} h={5} color={colors.darkGrayPrimary} />
-      </Center>
-    </Flex>
+    </form>
   );
 };
