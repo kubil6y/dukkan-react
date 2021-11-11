3885f9c6db8a7ddbc593a7c04201eae796d64ff0
Kubilay Humm <lieqb2@gmail.com>
2021-11-11 22:38:18 +0300

    ..

--- src/components/app/hooks/useUser.tsx
+++ src/components/app/hooks/useUser.tsx
@@ -39,7 +39,7 @@ export function useUser(): UseUser {
     queryClient.setQueryData(queryKeys.user, null);
 
     // TODO cancel user queries from react-query
-    // ...
+    // example ... queryClient.removeQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return {
